34a18ea443e7a88ce518c90cf1ce26e7c8d650e5
Yunhee Cho <yunheecho7@gmail.com>
2023-03-15 23:58:39 +0900

    I modified roots 1 and 2

--- func.py
+++ func.py
@@ -5,6 +5,9 @@ def add(x, y):
 def sub(x, y):
     return x - y
 
+def div(x, y):
+    return x / y
+
 def mult(x, y):
     return x*y
 
@@ -30,8 +33,8 @@ a = 1
 b = -3
 c = 1
 
-x1 = ... # TODO: write a code to compute the first root of the quadratic equation
-x2 = ... # TODO: then do the same for the second root
+x1 = div(add(neg(b), sqrt(sub(exp(b, 2), mult(4, mult(a, c))))), mult(2, a))# TODO: write a code to compute the first root of the quadratic equation
+x2 = div(sub(neg(b), sqrt(sub(exp(b, 2), mult(4, mult(a, c))))), mult(2, a)) # TODO: then do the same for the second root
 # Note: Make sure to remove the ellipsis (...) when you're writing your code
 
 print("First root:" + str(x1))
